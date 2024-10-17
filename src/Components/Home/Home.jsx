f4b9e6fc7d4788f97100e0ff3ee8a72e4202303c
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2024-10-17 18:14:20 -0300

    meu primeiro commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,37 @@
+import "../Home/Home.css"
+
+function Home() {
+  return (
+    <div className="App-pai">
+
+<div className="menu-container">
+  
+     <div className="menu">
+     <p>
+      Menu
+     </p>
+     <div className="bts">
+     <button id="bt3">Login</button>
+     <button id="bt4">Cadastro</button>
+     </div>
+     </div>
+     </div>
+  
+     <h1>Estoque P G!</h1>
+
+     <div className="App">
+      <h3>Quem nos somos:</h3>
+       <p> Somos uma empresa focada no ramo de gerenciamento de estoque.  <br></br> Fundada em 2024, facilitamos a vida dos nossos clientes com
+        a nossa simplicidade e objetividade.<br></br> Gerenciamos uma gama de produtos, de alimentos até itens de luxo. <br></br>Estamos sempre focando em nossa eficiencia,
+         satisfação dos nossos clientes e preços acessíveis para os fornecedores.
+
+     </p>
+
+     </div>
+    
+     
+    </div>
+  );
+}
+
+export default Home
