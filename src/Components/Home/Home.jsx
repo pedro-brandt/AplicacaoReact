67f3a2fda658056bf6d9ac79674e18bae2b20ced
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2024-10-21 22:35:18 +0000

    adição de imagem e tentativa de colocar ela ao lado do texto

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,20 +18,18 @@ function Home() {
      </div>
   
      <h1>Estoque P G!</h1>
-
+     <br></br>
      <div className="App">
       <h3>Quem nos somos:</h3>
-       <p> Somos uma empresa focada no ramo de gerenciamento de estoque.  <br></br> Fundada em 2024, facilitamos a vida dos nossos clientes com
+      <br></br>
+       <p><img src="https://roldao.com.br/wp-content/uploads/2020/03/Gest%C3%A3o-de-estoque-Blog-do-Rold%C3%A3o.jpg" width={600} height={400}></img>Somos uma empresa focada no ramo de gerenciamento de estoque.  <br></br> Fundada em 2024, facilitamos a vida dos nossos clientes com
         a nossa simplicidade e objetividade.<br></br> Gerenciamos uma gama de produtos, de alimentos até itens de luxo. <br></br>Estamos sempre focando em nossa eficiencia,
          satisfação dos nossos clientes e preços acessíveis para os fornecedores.
 
      </p>
-
-     </div>
-    
      
-    </div>
+     </div>
+     </div>
   );
 }
 
-export default Home
