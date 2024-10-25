3dcab3dbc868e03578745bf3e245da53b7eb270e
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2024-10-25 19:07:48 +0000

    texto, imagens e css butao

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -25,9 +25,37 @@ export default function Home() {
        <p><img src="https://roldao.com.br/wp-content/uploads/2020/03/Gest%C3%A3o-de-estoque-Blog-do-Rold%C3%A3o.jpg" width={600} height={400}></img>Somos uma empresa focada no ramo de gerenciamento de estoque.  <br></br> Fundada em 2024, facilitamos a vida dos nossos clientes com
         a nossa simplicidade e objetividade.<br></br> Gerenciamos uma gama de produtos, de alimentos até itens de luxo. <br></br>Estamos sempre focando em nossa eficiencia,
          satisfação dos nossos clientes e preços acessíveis para os fornecedores.
-
+         <br></br>
+         <br></br>
+         Nós nos orgulhamos de sermos uma das 10 melhores empresas de gerenciamento de produtos da américa latina com mais de 100.00 clientes.<br></br>
+         <br></br>
+         <p>
+          Veja aseguir algum de nossos produtos:
+         </p>
      </p>
-     
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <h4>
+          Veja aseguir algum de nossos produtos:
+         </h4>
+         <br></br>
+         <br></br>
+          <img src="https://jasminealimentos.com/wp-content/uploads/2017/07/como-escolher-os-melhores-alimentos-1.jpg" width={400} height={200}></img>
+          <img src="https://bahiaeconomica.com.br/wp/wp-content/uploads/2024/09/patio-da-vports-recebe-carros-importados-2075839.webp" width={400} height={200}></img> 
+          <img src="https://blog.damasemijoias.com.br/wp-content/uploads/2017/08/como-organizar-um-estoque-de-semijoias-aprenda-810x485.jpeg" width={400} height={200}></img>
      </div>
      </div>
   );
