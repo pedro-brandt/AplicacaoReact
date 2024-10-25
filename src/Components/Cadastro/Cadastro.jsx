861f0c76663e7a0909175bd81e99c547a9f0eff8
Pedro <105327849+pedro-brandt@users.noreply.github.com>
2024-10-25 21:14:53 +0000

    ajustes com flex box das imagens

---
+++ src/Components/Cadastro/Cadastro.jsx
@@ -0,0 +1,14 @@
+export default function Cadastro() {
+  return (
+    <div class name="cadastro">
+        <h2>Cadastro</h2>
+
+        <form>
+        <label for="nome">Nome:</label>
+        <label for="senha">Senha:</label>
+        <label for="confirmação">Senha:</label>
+        <input type="submit" value="Salvar"></input>
+        </form>
+    </div>
+  )
+}
