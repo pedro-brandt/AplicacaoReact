3eb940a9c25805efb490cb0f103f256d8b468f42
pedro-brandt <pedro.2020107111@unicap.br>
2024-11-12 14:02:10 -0300

    muda div login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div className="login">
+        <div className="login1">
             <h2 clasName="LoginT">Login</h2>
             <form>
-                <p className="txInformacoes">Digite os seus dados de acesso.</p>
+                <p id="txInformacoes">Digite os seus dados de acesso.</p>
                 <label className="labelNomeL" htmlFor="nome">Nome:<textarea minlength="20" maxlength="27">
                 </textarea></label>
                 <label className="labelSenhaL" htmlFor="senha">Senha:<textarea minlength="6" maxlength="8">
